909c8fded4da1c03a66fd360718438eeedda1b36
yunxiaozou <dfs>
2016-04-12 22:53:05 -0400

    3231

--- 015/src/Solution.java
+++ 015/src/Solution.java
@@ -21,16 +21,19 @@ public class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && (nums[i] == nums[i-1])) continue;
             int target = nums[i] * (-1);
             int left = i+1;
             int right = nums.length-1;
             while (left < right) {
                 if (nums[left] + nums[right] == target) {
                     ArrayList<Integer> current = new ArrayList<>();
-                    current.add(i);
-                    current.add(left);
-                    current.add(right);
+                    current.add(nums[i]);
+                    current.add(nums[left]);
+                    current.add(nums[right]);
                     result.add(current);
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
                     left++;
                     right--;
                 } else if (nums[left] + nums[right] > target) {
