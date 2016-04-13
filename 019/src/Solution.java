cc13b73cc16599020ed54d81ebf9e22ac569a8c8
yunxiaozou <dfs>
2016-04-13 09:21:29 -0400

    3231

--- 019/src/Solution.java
+++ 019/src/Solution.java
@@ -1,6 +1,22 @@
+
+
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //TODO
-        return null;
+        ListNode p = head;
+        while (p != null && n != 0){
+            p = p.next;
+            n--;
+        }
+        if (p == null) {
+            return head.next;
+        }
+        ListNode q = head;
+        while (p.next != null) {
+            p = p.next;
+            q = q.next;
+        }
+
+        q.next = q.next.next;
+        return head;
     }
 }
