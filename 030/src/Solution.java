9b93481dacacac919089330fdbaf8656a99b660d
yunxiaozou <dfs>
2016-04-13 23:29:11 -0400

    3231

---
+++ 030/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+/**
+ * Created by yunxiaozou on 4/13/16.
+ * You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+ For example, given:
+ s: "barfoothefoobarman"
+ words: ["foo", "bar"]
+
+ You should return the indices: [0,9].
+ (order does not matter).
+ */
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+
+}
