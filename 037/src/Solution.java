33810aab393e110122814ed039473d641355cd07
yunxiaozou <dfs>
2016-04-15 11:53:13 -0400

    342342

---
+++ 037/src/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Created by yunxiaozou on 4/15/16.
+ *
+ *
+ *
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+ Empty cells are indicated by the character '.'.
+
+ You may assume that there will be only one unique solution.
+ */
+public class Solution {
+    public void solveSudoku(char[][] board) {
+
+    }
+}
