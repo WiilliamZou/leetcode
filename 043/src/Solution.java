6badd74e09819e2933e46678470fc0c5ddebcef1
yunxiaozou <dfs>
2016-04-16 19:59:47 -0400

    342342

---
+++ 043/src/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Created by yunxiaozou on 4/16/16.
+ *
+ * Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+ Note:
+ The numbers can be arbitrarily large and are non-negative.
+ Converting the input string to integer is NOT allowed.
+ You should NOT use internal library such as BigInteger.
+ */
+public class Solution {
+    public String multiply(String num1, String num2) {
+        return null;
+    }
+}
