1b6cbe87725d545e636e5f034abad45eae264038
yunxiaozou <dfs>
2016-04-16 19:01:20 -0400

    342342

---
+++ 041/src/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Created by yunxiaozou on 4/16/16.
+ *
+ * Given an unsorted integer array, find the first missing positive integer.
+
+ For example,
+ Given [1,2,0] return 3,
+ and [3,4,-1,1] return 2.
+
+ Your algorithm should run in O(n) time and uses constant space.
+ */
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        return 0;
+    }
+}
