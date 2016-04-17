6567684c0463417076691adf6f842e932472a3d2
yunxiaozou <dfs>
2016-04-16 22:53:03 -0400

    342342

--- 043/src/Solution.java
+++ 043/src/Solution.java
@@ -10,6 +10,24 @@
  */
 public class Solution {
     public String multiply(String num1, String num2) {
-        return null;
+        int[] output = new int[num1.length()+ num2.length()];
+        for (int i = num1.length()-1; i >= 0; i--) {
+            for (int j = num2.length()-1; j >= 0; j--) {
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+                int temp = digit1 * digit2;
+                output[i+j] += (temp + output[i+j+1])/10;
+                output[i+j+1] = (temp + output[i+j+1])%10;
+
+            }
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (int digit : output) {
+            if (buffer.length() != 0 || digit != 0)
+                buffer.append(digit);
+        }
+        if (buffer.length() == 0)
+            buffer.append(0);
+        return buffer.toString();
     }
 }
