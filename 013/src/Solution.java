b6d1daf60f592249ad6334e861e6e6cd4228a136
yunxiaozou <dfs>
2016-04-21 21:20:47 -0400

    342342

---
+++ 013/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+/**
+ * Created by yunxiaozou on 4/21/16.
+ */
+public class Solution {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0)
+            return -1;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int value = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i <  s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1)))
+                value -= map.get(s.charAt(i));
+            else
+                value += map.get(s.charAt(i));
+        }
+        return value;
+    }
+}
---
+++ 047/src/Solution.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by yunxiaozou on 4/21/16.
+ */
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        return null;
+
+    }
+}
