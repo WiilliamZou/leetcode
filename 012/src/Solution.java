26bb382751c187e9681956c12d2e95d3ca1acb10
yunxiaozou <dfs>
2016-04-21 20:47:46 -0400

    342342

---
+++ 012/src/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yunxiaozou on 4/21/16.
+ */
+public class Solution {
+    public String intToRoman(int num) {
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[]    value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int base = 0;
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < roman.length; i++) {
+            if ((base = num/value[i]) != 0) {
+                while (base--!=0) result.append(roman[i]);
+                num = num % value[i];
+            }
+        }
+        return result.toString();
+    }
+}
--- 012/src/Solution2.java
+++ 012/src/Solution2.java
@@ -4,7 +4,7 @@
 public class Solution2 {
     public String intToRoman(int num) {
         String data[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        int value[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1}, base = -1;
+        int value[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1}, base = 0;
         StringBuilder result = new StringBuilder();
 
         for(int i = 0;i < data.length;i++){
