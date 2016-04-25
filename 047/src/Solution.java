a4bebbc6366c9f3c9e29409a5b7d450dfbdd3019
yunxiaozou <dfs>
2016-04-25 01:52:01 -0400

    342342

--- 047/src/Solution.java
+++ 047/src/Solution.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -8,7 +9,34 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null && nums.length == 0)
+            return result;
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], numMap.getOrDefault(nums[i], 0) + 1);
+        }
+        List<Integer> current = new ArrayList<>();
+        dfs(nums, result, current, numMap);
+        return result;
 
     }
+
+    private void dfs(int[] nums, List<List<Integer>> result, List<Integer> current, HashMap<Integer, Integer> numMap) {
+        if (nums.length == current.size()) {
+            result.add(new ArrayList<>(current));
+        } else {
+            for (int currentNum : nums) {
+                if (numMap.getOrDefault(currentNum, 0) != 0) {
+                    current.add(currentNum);
+                    numMap.put(currentNum, numMap.getOrDefault(currentNum,0) - 1);
+                    dfs(nums, result, current, numMap);
+                    numMap.put(currentNum, numMap.getOrDefault(currentNum,0) + 1);
+                    current.remove(current.size()-1);
+                }
+            }
+        }
+    }
+
+
 }
---
+++ 047/src/SolutionTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by yunxiaozou on 4/25/16.
+ */
+public class SolutionTest {
+    @Test
+    public void testSolution() {
+        Solution sol = new Solution();
+        sol.permuteUnique(new int[]{1,1});
+    }
+
+}
