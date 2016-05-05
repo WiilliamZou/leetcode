aec26507e077bccd83ec08f63edff61f0d17c885
yunxiaozou <dfs>
2016-05-05 18:12:06 -0400

    342342

--- 050/src/Solution.java
+++ 050/src/Solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public double myPow(double x, int n) {
-        if (x == 0 || x == 1) {
-            return x;
-        } else if (x == -1) {
-            return (n % 2 == 0) ? 1 : -1;
-        } else if (n == 0 ) {
-            return 1;
-        } else if (n < 0){
-            return 1 / myPow(x, -n);
-        } else if (n % 2 == 0) {
-            double result = myPow(x, n/2);
-            return result * result;
-        } else {
-            return x * myPow(x, n-1);
+        if (n == 0 ) return 1;
+        if (x == 0) return 0;
+        int signN = (n > 0)? 1 : -1;
+        long tempN = Math.abs(n);
+        double result = 1;
+        if (tempN % 2 == 1) {
+            result = x;
+            tempN--;
         }
-
+        double tempResult = myPow(x, (int)(tempN/2));
+        result *= tempResult * tempResult;
+        if (signN < 0)
+            result = 1 / result;
+        return result;
     }
 }
