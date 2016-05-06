b303dac5ec72ed3f1d3dc144894b3a294ee7ce82
yunxiaozou <dfs>
2016-05-05 21:36:45 -0400

    342342

---
+++ 053/src/Solution2.java
@@ -0,0 +1,38 @@
+public class Solution2 {
+    public int maxSubArrayRec(int[] nums, int i, int j) {
+        if(i==j)
+            return nums[i];
+        int mid = (i + j) / 2;
+        int leftMax = maxSubArrayRec(nums,i,mid);
+        int rightMax = maxSubArrayRec(nums,mid+1,j);
+        int subMax = Math.max(leftMax,rightMax);
+
+        int leftContiguousMax = nums[mid];
+        int leftSum = nums[mid];
+        for(int k=mid-1; k>=i; k--) {
+            leftSum += nums[k];
+            if(leftSum > leftContiguousMax)
+                leftContiguousMax = leftSum;
+        }
+
+        if(leftContiguousMax <= 0)
+            return subMax;
+
+        int rightContigousMax = nums[mid+1];
+        int rightSum = nums[mid+1];
+        for(int k=mid+2; k<=j; k++) {
+            rightSum += nums[k];
+            if(rightSum > rightContigousMax)
+                rightContigousMax = rightSum;
+        }
+
+        if(rightContigousMax <= 0)
+            return subMax;
+
+        return Math.max(subMax,leftContiguousMax+rightContigousMax);
+    }
+
+    public int maxSubArray(int[] nums) {
+        return maxSubArrayRec(nums,0,nums.length-1);
+    }
+}
