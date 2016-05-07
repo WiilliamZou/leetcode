2d52bccd12930d66e38094b923f9b31b4bf04783
yunxiaozou <dfs>
2016-05-07 14:22:54 -0400

    342342

--- 057/src/Solution.java
+++ 057/src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -5,6 +8,26 @@ import java.util.List;
  */
 public class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        return null;
+        intervals.add(newInterval);
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.start-o2.start;
+            }
+        });
+        Interval hold = intervals.get(0);
+        ArrayList<Interval> result = new ArrayList<>();
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval current = intervals.get(i);
+            if (current.start <= hold.end) {
+                hold.end = Math.max(hold.end, current.end);
+            } else {
+                result.add(hold);
+                hold = current;
+            }
+
+        }
+        result.add(hold);
+        return result;
     }
 }
