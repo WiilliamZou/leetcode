d275acbee2354137ff94ce754d679cb28e81f756
yunxiaozou <dfs>
2016-05-07 12:59:49 -0400

    342342

---
+++ 056/src/Interval.java
@@ -0,0 +1,14 @@
+/**
+ * Created by yunxiaozou on 5/7/16.
+ */
+public class Interval {
+    int start;
+    int end;
+    public Interval() {
+        this(0,0);
+    }
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ 056/src/Solution2.java
@@ -0,0 +1,34 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution2 {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.isEmpty())
+            return intervals;
+
+        Collections.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) {
+                // TODO Auto-generated method stub
+                return a.start - b.start;
+            }
+        });
+        List<Interval> ans = new LinkedList<Interval>();
+        Interval hold = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval current = intervals.get(i);
+            if (hold.end >= current.start) {
+                hold.end = Math.max(current.end, hold.end);
+            } else {
+                ans.add(hold);
+                hold = current;
+            }
+        }
+
+        if (!ans.contains(hold))
+            ans.add(hold);
+
+        return ans;
+    }
+}
