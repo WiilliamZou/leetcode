1be6fae4052d060c8fd66e18d5d23b8f96ceaff5
yunxiaozou <dfs>
2016-05-07 14:27:52 -0400

    342342

---
+++ 058/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) return 0;
+        s = s.trim();
+        if (s.length() == 0) return 0;
+        int count = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == ' ')
+                return count;
+            count++;
+        }
+        return count;
+    }
+}
