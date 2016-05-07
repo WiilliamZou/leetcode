88723146f78239c21968af54e7d005bcd13080bc
yunxiaozou <dfs>
2016-05-07 14:11:27 -0400

    342342

--- 056/src/Solution2.java
+++ 056/src/Solution2.java
@@ -20,7 +20,6 @@ public class Solution2 {
             Interval current = intervals.get(i);
             if (hold.end >= current.start) {
                 hold.end = Math.max(current.end, hold.end);
-
             } else {
                 ans.add(hold);
                 hold = current;
---
+++ 057/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+/**
+ * Created by yunxiaozou on 5/7/16.
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        return null;
+    }
+}
