c2e6e4274248aaa88d8be2aa59a1ed3db91a3f0c
yunxiaozou <dfs>
2016-05-09 15:09:30 -0400

    342342

---
+++ 066/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * Created by yunxiaozou on 5/9/16.
+ */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int[] result = new int[digits.length + 1];
+        int carry = 1;
+        for (int i = digits.length-1; i >= 0; i--) {
+            int temp = digits[i] + carry;
+            if (temp > 9) {
+                temp -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+            result[i+1] = temp;
+        }
+        if (carry == 1)
+            result[0] = 1;
+        if (result[0] == 0) {
+            return Arrays.copyOfRange(result, 1, result.length);
+        } else {
+            return result;
+        }
+    }
+}
