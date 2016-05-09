842c02d94a09974f6b611d09f1528d31eaac55f4
yunxiaozou <dfs>
2016-05-09 14:59:44 -0400

    342342

--- 065/src/Solution.java
+++ 065/src/Solution.java
@@ -1,5 +1,40 @@
 public class Solution {
     public boolean isNumber(String s) {
-        return false;
+        if (s == null ) return false;
+        s = s.trim();
+        boolean hasFirst, hasDot, hasDigit, hasE;
+        hasDigit = hasFirst = hasDot = hasE = false;
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                hasFirst = hasDigit = true;
+            } else {
+                switch (c) {
+                    case '+':
+                    case '-':
+                        if (hasFirst) {
+                            return false;
+                        }
+                        hasFirst = true;
+                        break;
+                    case '.':
+                        if (hasDot)
+                            return false;
+                        hasDot = true;
+                        hasFirst = true;
+                        break;
+                    case 'e':
+                        if (hasE || !hasDigit)
+                            return false;
+                        hasE = true;
+                        hasDigit = false;
+                        hasDot = true;
+                        hasFirst = false;
+                        break;
+                    default:
+                        return false;
+                }
+            }
+        }
+        return hasDigit;
     }
 }
