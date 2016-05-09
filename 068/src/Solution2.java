c08fafb69e49b351c5c0c021fc959cc5a95e3be6
yunxiaozou <dfs>
2016-05-09 16:26:49 -0400

    342342

--- 068/src/Solution2.java
+++ 068/src/Solution2.java
@@ -7,23 +7,20 @@ import java.util.List;
 public class Solution2 {
         public List<String> fullJustify(String[] words, int L) {
             List<String> lines = new ArrayList<String>();
-
-            int index = 0;
-            while (index < words.length) {
-                int count = words[index].length();
-                int next = index + 1;
+            int first = 0;
+            while (first < words.length) {
+                int count = words[first].length();
+                int next = first + 1;
                 while (next < words.length) {
                     if (words[next].length() + count + 1 > L) break;
                     count += words[next].length() + 1;
                     next++;
                 }
-                //index means the first word in current line, next-1 means the last word in current line.
-
                 StringBuilder builder = new StringBuilder();
-                int numWords = next - index - 1;
+                int numWords = next - first - 1;
                 // if next line or number of words in the line is 1, left-justified
                 if (next == words.length || numWords == 0) {
-                    for (int i = index; i < next; i++) {
+                    for (int i = first; i < next; i++) {
                         builder.append(words[i] + " ");
                     }
                     builder.deleteCharAt(builder.length() - 1);
@@ -34,20 +31,18 @@ public class Solution2 {
                     // middle justified
                     int spaces = (L - count) / numWords;
                     int r = (L - count) % numWords;
-                    for (int i = index; i < next; i++) {
+                    for (int i = first; i < next; i++) {
                         builder.append(words[i]);
                         if (i < next - 1) {
-                            for (int j = 0; j <= (spaces + ((i - index) < r ? 1 : 0)); j++) {
+                            for (int j = 0; j <= (spaces + ((i - first) < r ? 1 : 0)); j++) {
                                 builder.append(" ");
                             }
                         }
                     }
                 }
                 lines.add(builder.toString());
-                index = next;
+                first = next;
             }
-
-
             return lines;
         }
 
