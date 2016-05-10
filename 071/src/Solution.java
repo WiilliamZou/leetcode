c3b9ec9e256394d7f7950782951c8b4496d992bd
yunxiaozou <dfs>
2016-05-10 13:24:51 -0400

    342342

---
+++ 071/src/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public String simplifyPath(String path) {
+        return null;
+    }
+}
---
+++ 071/src/Solution3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * Created by yunxiaozou on 5/10/16.
+ */
+public class Solution3 {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
+        }
+        String res = "";
+        for (String dir : stack) res = "/" + dir + res;
+        return res.isEmpty() ? "/" : res;
+    }
+}
