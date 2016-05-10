9332545cc173ebc63a7a453329213e185e0d5728
yunxiaozou <dfs>
2016-05-10 14:35:33 -0400

    342342

--- 071/src/Solution.java
+++ 071/src/Solution.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Solution {
     public String simplifyPath(String path) {
-        return null;
+        Deque<String> stack = new LinkedList<>();
+        String res = "";
+        Set<String> skip = new HashSet<>(Arrays.asList(".", "..", ""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
+        }
+
+        for (String dir : stack) {
+            res = "/" + dir + res;
+        }
+        if (res.isEmpty())
+            res = "/";
+        return res;
     }
 }
