c609e6845649e88fa3533f10b3bd3627eaff3839
yunxiaozou <dfs>
2016-05-11 16:02:25 -0400

    342342

--- 078/src/Solution.java
+++ 078/src/Solution.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) return result;
+        Arrays.sort(nums);
+        List<Integer> current = new ArrayList<>();
+        dfs(nums, 0, result, current);
+        return result;
+    }
+
+    private void dfs(int[] nums, int index, List<List<Integer>> result, List<Integer> current) {
+        if (index == nums.length) result.add(new ArrayList<>(current));
+        else {
+            dfs(nums, index+1, result, current);
+            current.add(nums[index]);
+            dfs(nums, index+1, result, current);
+            current.remove(current.size()-1);
+
+        }
     }
 }
---
+++ 079/src/Solution.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yunxiaozou on 5/11/16.
+ */
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
