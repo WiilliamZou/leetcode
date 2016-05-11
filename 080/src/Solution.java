62382a1b7c6eaa4c15dc894deef3b996f7b22abc
yunxiaozou <dfs>
2016-05-11 16:23:06 -0400

    342342

---
+++ 080/src/Solution.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yunxiaozou on 5/11/16.
+ */
+public class Solution {
+}
---
+++ 080/src/Solution2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yunxiaozou on 5/11/16.
+ */
+public class Solution2 {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i-2])
+                nums[i++] = n;
+        return i;
+    }
+}
