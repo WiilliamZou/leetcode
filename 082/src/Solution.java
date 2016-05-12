5555995b58add449dda15bc7c3e4c490c91f91c5
yunxiaozou <dfs>
2016-05-12 17:19:55 -0400

    342

--- 082/src/Solution.java
+++ 082/src/Solution.java
@@ -1,5 +1,28 @@
 /**
- * Created by yunxiaozou on 5/12/16.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
 public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        ListNode prev = fakeHead;
+        while(prev.next != null) {
+            ListNode current = prev.next;
+            if (current.next != null && current.val == current.next.val) {
+                ListNode next = current;
+                while (next != null && next.val == current.val) {
+                    next = next.next;
+                }
+                prev.next = next;
+            } else {
+                prev = current;
+            }
+        }
+        return fakeHead.next;
+    }
 }
