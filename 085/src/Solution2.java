346cbba433a618bf9b1903b744c21ef4fa507d0d
yunxiaozou <yunxiaozou@gmail.com>
2016-05-14 18:28:40 -0400

    342

--- 085/src/Solution2.java
+++ 085/src/Solution2.java
@@ -3,13 +3,9 @@ import java.util.Stack;
 public class Solution2 {
     public int maximalRectangle(char[][] matrix) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
-
         int[] height = new int[matrix[0].length];
-        for(int i = 0; i < matrix[0].length; i ++){
-            if(matrix[0][i] == '1') height[i] = 1;
-        }
-        int result = largestInLine(height);
-        for(int i = 1; i < matrix.length; i ++){
+        int result = 0;
+        for(int i = 0; i < matrix.length; i ++){
             resetHeight(matrix, height, i);
             result = Math.max(result, largestInLine(height));
         }
@@ -25,21 +21,18 @@ public class Solution2 {
     }
 
     public int largestInLine(int[] height) {
-        if(height == null || height.length == 0) return 0;
-        int len = height.length;
-        Stack<Integer> s = new Stack<Integer>();
-        int maxArea = 0;
-        for(int i = 0; i <= len; i++){
-            int h = (i == len ? 0 : height[i]);
-            if(s.isEmpty() || h >= height[s.peek()]){
-                s.push(i);
-            }else{
-                int tp = s.pop();
-                maxArea = Math.max(maxArea, height[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
-                i--;
+        Stack<Integer> stack = new Stack<>();
+        int max = 0, i = 0, n = height.length;
+        while (i < n) {
+            while (!stack.empty() && height[i] < height[stack.peek()]) {
+                max = Math.max(max, height[stack.pop()] * (i - (stack.empty() ? 0 : stack.peek()+1)));
             }
+            stack.push(i++);
         }
-        return maxArea;
+        while (!stack.empty()) {
+            max = Math.max(max, height[stack.pop()] * (i - (stack.empty() ? 0 : stack.peek()+1)));
+        }
+        return max;
     }
 }
 
