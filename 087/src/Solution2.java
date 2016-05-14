68332c0ac2cb0f45274a92be7251b9f1e841b010
yunxiaozou <yunxiaozou@gmail.com>
2016-05-14 19:47:17 -0400

    342

--- 087/src/Solution2.java
+++ 087/src/Solution2.java
@@ -4,13 +4,13 @@ public class Solution2 {
         if(len!=s2.length()) return false;
         if(len==0) return true;
         boolean[][][] isScr = new boolean[len][len][len];
-        for(int i = 0; i < len; i++) { //length of current substring, 0 means length==1
-            for(int j = 0; j + i < len; j++) { //start point of current substring at s1.
-                for(int k = 0; k + i < len; k++) { //start point of current substring at s2.
-                    if(i==0) isScr[i][j][k] = s1.charAt(j)==s2.charAt(k);
-                    for(int m = 0; m < i; m++) {
-                        if(isScr[m][j][k] && isScr[i-(m+1)][j+m+1][k+m+1]) isScr[i][j][k] = true;
-                        else if(isScr[m][j][k+i-m] && isScr[i-(m+1)][j+m+1][k]) isScr[i][j][k] = true;
+        for(int l = 0; l < len; l++) { //length of current substring, 0 means length==1
+            for(int i = 0; i + l < len; i++) { //start point of current substring at s1.
+                for(int j = 0; j + l < len; j++) { //start point of current substring at s2.
+                    if(l==0) isScr[l][i][j] = s1.charAt(i)==s2.charAt(j);
+                    for(int m = 0; m < l; m++) {
+                        if(isScr[m][i][j] && isScr[l-(m+1)][i+m+1][j+m+1]) isScr[l][i][j] = true;
+                        else if(isScr[m][i][j+l-m] && isScr[l-(m+1)][i+m+1][j]) isScr[l][i][j] = true;
                     }
                 }
             }
