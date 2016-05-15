27dd871a8667ed825db64a86159dd23bf43f7cce
yunxiaozou <yunxiaozou@gmail.com>
2016-05-15 13:09:55 -0400

    342

--- 093/src/Solution2.java
+++ 093/src/Solution2.java
@@ -6,29 +6,29 @@ import java.util.List;
  */
 public class Solution2 {
     public List<String> restoreIpAddresses(String s) {
-        List<String> ret = new LinkedList<>();
-        int[] path = new int[4];
-        helper(ret, s, 0,  path, 0);
-        return ret;
+        List<String> result = new LinkedList<>();
+        int[] curr = new int[4];
+        dfs(result, s, 0,  curr, 0);
+        return result;
     }
 
-    void helper(List<String> acc, String s, int idx, int[] path,  int segment){
+    void dfs(List<String> acc, String s, int idx, int[] curr, int segment){
         if(segment == 4 && idx == s.length() ){
-            acc.add(path[0] + "." + path[1] + "."+ path[2] + "." + path[3]);
+            acc.add(curr[0] + "." + curr[1] + "."+ curr[2] + "." + curr[3]);
             return ;
         }else if(segment == 4 || idx == s.length() ){
             return ;
         }
-
+        // len 代表当前segment 的长度。
         for(int len = 1; len <= 3 && idx + len <= s.length() ; len ++){
             int val = Integer.parseInt(s.substring(idx, idx + len));
             // range check, no leading 0.
             if(val > 255 || len >= 2  && s.charAt(idx) == '0')
                 break;
 
-            path[segment] = val;
-            helper(acc, s, idx + len, path, segment + 1);
-            path[segment] = -1; // for debug.
+            curr[segment] = val;
+            dfs(acc, s, idx + len, curr, segment + 1);
+            curr[segment] = -1; // for debug.
         }
     }
 }
