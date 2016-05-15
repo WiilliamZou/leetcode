48078bb0f7d7a1db89e5ebd10a927369082219bf
yunxiaozou <yunxiaozou@gmail.com>
2016-05-15 17:21:14 -0400

    342

---
+++ 095/src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yunxiaozou on 5/15/16.
+ */
+
+import java.util.List;
+
+
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
---
+++ 095/src/Solution2.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yunxiaozou on 5/15/16.
+ */
+public class Solution2 {
+    public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1,n);
+    }
+
+    public List<TreeNode> generateTrees(int start,int end){
+        List<TreeNode> trees = new ArrayList<TreeNode>();
+        if(start>end){  trees.add(null); return trees;}
+
+        for(int rootValue=start;rootValue<=end;rootValue++){
+            List<TreeNode> leftSubTrees=generateTrees(start,rootValue-1);
+            List<TreeNode> rightSubTrees=generateTrees(rootValue+1,end);
+
+            for(TreeNode leftSubTree:leftSubTrees){
+                for(TreeNode rightSubTree:rightSubTrees){
+                    TreeNode root=new TreeNode(rootValue);
+                    root.left=leftSubTree;
+                    root.right=rightSubTree;
+                    trees.add(root);
+                }
+            }
+        }
+        return trees;
+    }
+}
