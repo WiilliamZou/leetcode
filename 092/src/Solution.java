212d5777be213d12ae9e4487d90164db95a437ff
yunxiaozou <yunxiaozou@gmail.com>
2016-05-15 12:45:03 -0400

    342

--- 092/src/Solution.java
+++ 092/src/Solution.java
@@ -8,6 +8,26 @@
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
+        ListNode prev = new ListNode(0);
+        prev.next = head;
+        int k = n - m;
+        int j = m;
+        while (j > 1) {
+            prev = prev.next;
+            j--;
+        }
+
+        ListNode curr = prev.next;
+        while (k > 0) {
+            ListNode temp = curr.next;
+            curr.next = curr.next.next;
+            temp.next = prev.next;
+            prev.next = temp;
+            k--;
+        }
+        if (m == 1)
+            head = prev.next;
+        return head;
+
     }
 }
