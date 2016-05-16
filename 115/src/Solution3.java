9ba798401ab588a90d8da4a7e993743ab9988800
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 19:03:16 -0400

    342

---
+++ 115/src/Solution3.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+/**
+ * Created by yunxiaozou on 5/16/16.
+ */
+public class Solution3 {
+    public void flatten(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) {return;}
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            node.left = null;
+            if (right != null) {stack.push(right);}
+            if (left != null) {stack.push(left);}
+            if (left != null) {
+                node.right = left;
+            } else {
+                node.right = stack.isEmpty()? right: stack.peek();
+            }
+        }
+    }
+}
+
+
