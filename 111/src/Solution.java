65d4b2b8e028400cd216ddcea6c4c97757220888
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 17:00:40 -0400

    342

--- 111/src/Solution.java
+++ 111/src/Solution.java
@@ -3,9 +3,14 @@
  */
 public class Solution {
     public int minDepth(TreeNode root) {
-        return 0
+        return traverse(root, 0);
     }
 
-
+    private int traverse(TreeNode node, int height) {
+        if (node == null) return height;
+        if (node.left == null) return traverse(node.right, height+1);
+        else if (node.right == null) return traverse(node.left, height+1);
+        else return Math.min(traverse(node.left,height+1), traverse(node.right, height+1));
+    }
 
 }
