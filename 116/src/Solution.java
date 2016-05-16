f580f4f1c00b0e00b270997017397872ff6e19f7
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 19:47:40 -0400

    342

--- 116/src/Solution.java
+++ 116/src/Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
-
+        if (root == null) return;
+        while (root.left != null) {
+            TreeLinkNode node = root;
+            while (node != null) {
+                node.left.next = node.right;
+                if (node.next != null) {
+                    node.right.next = node.next.left;
+                }
+                node = node.next;
+            }
+            root = root.left;
+        }
     }
 }
