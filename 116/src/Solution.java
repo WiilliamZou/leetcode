709af718de8b38dbb028c3b9742ca5ac68eb9482
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 19:33:31 -0400

    342

---
+++ 116/src/Solution.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yunxiaozou on 5/16/16.
+ */
+public class Solution {
+}
---
+++ leetcodeTree/src/TreeLinkNode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yunxiaozou on 5/16/16.
+ */
+public class TreeLinkNode {
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+    int val;
+    TreeLinkNode(int x) {val = x;}
+}
