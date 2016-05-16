e854d3b03375befdede86e7138ca275d760382f2
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 19:08:49 -0400

    342

--- 115/src/Solution3.java
+++ 115/src/Solution3.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 public class Solution3 {
     public void flatten(TreeNode root) {
         // write your code here
-        // 这个算法貌似和preorder traversal 有点相似
+        // 这个算法貌似和preorder traversal 能联系上
         Stack<TreeNode> stack = new Stack<>();
         if (root == null) {return;}
         stack.push(root);
