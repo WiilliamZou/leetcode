0fc5c0190b6cd5c4cc82351cd408cac163a0c67a
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 20:14:59 -0400

    342

--- 117/src/Solution.java
+++ 117/src/Solution.java
@@ -1,5 +1,19 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
-
+        if (root == null) return;
+        for (TreeLinkNode head = root; head != null;) {
+            TreeLinkNode nextFakeHead  = new TreeLinkNode(0), nextNode = nextFakeHead;
+            for (TreeLinkNode node = head; node != null; node = node.next) {
+                if (node.left != null) {
+                    nextNode.next = node.left;
+                    nextNode = nextNode.next;
+                }
+                if (node.right != null) {
+                    nextNode.next = node.right;
+                    nextNode = nextNode.next;
+                }
+            }
+            head = nextFakeHead.next;
+        }
     }
 }
