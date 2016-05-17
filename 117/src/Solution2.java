3e0d7271fd40f30c49f6ac0e82f8c926c8fb682a
yunxiaozou <yunxiaozou@gmail.com>
2016-05-16 20:10:39 -0400

    342

--- 117/src/Solution2.java
+++ 117/src/Solution2.java
@@ -4,18 +4,18 @@
 public class Solution2 {
     public void connect(TreeLinkNode root) {
         for (TreeLinkNode head = root; head != null; ) {
-            TreeLinkNode nextHead = new TreeLinkNode(0), nextTail = nextHead;
+            TreeLinkNode nextFakeHead = new TreeLinkNode(0), nextNode = nextFakeHead;
             for (TreeLinkNode node = head; node != null; node = node.next) {
                 if (node.left != null) {
-                    nextTail.next = node.left;
-                    nextTail = node.left;
+                    nextNode.next = node.left;
+                    nextNode = node.left;
                 }
                 if (node.right != null) {
-                    nextTail.next = node.right;
-                    nextTail = node.right;
+                    nextNode.next = node.right;
+                    nextNode = node.right;
                 }
             }
-            head = nextHead.next;
+            head = nextFakeHead.next;
         }
     }
 }
