dd55532d7b89b3be2293bc13716c1f2b58c44817
yunxiaozou <yunxiaozou@gmail.com>
2016-05-18 10:29:31 -0400

    342

--- 124/src/Solution.java
+++ 124/src/Solution.java
@@ -2,7 +2,18 @@
  * Created by yunxiaozou on 5/18/16.
  */
 public class Solution {
+    int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        return 0;
+        if (root == null) return 0;
+        traverse(root);
+        return max;
+    }
+
+    private int traverse(TreeNode node) {
+        if (node == null) return Integer.MIN_VALUE;
+        int left = Math.max(traverse(node.left), 0);
+        int right = Math.max(traverse(node.right), 0);
+        max = Math.max(max, node.val+left+right);
+        return node.val + Math.max(left, right);
     }
 }
