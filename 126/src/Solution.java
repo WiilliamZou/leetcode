d58c85b5b891e5a420aaed505c8351e699c43662
yunxiaozou <yunxiaozou@gmail.com>
2016-05-18 11:38:56 -0400

    342

--- 126/src/Solution.java
+++ 126/src/Solution.java
@@ -1,8 +1,8 @@
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
-        return null;
+       return null;
+
     }
 }
