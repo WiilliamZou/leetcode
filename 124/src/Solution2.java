f78a5a2a1cece7457cc76e37087fc849b6697564
yunxiaozou <yunxiaozou@gmail.com>
2016-05-18 10:21:49 -0400

    342

--- 124/src/Solution2.java
+++ 124/src/Solution2.java
@@ -5,15 +5,15 @@ public class Solution2 {
     int max = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
-        helper(root);
+        traverse(root);
         return max;
     }
 
-    public int helper(TreeNode root) {
+    public int traverse(TreeNode root) {
         if(root == null)
             return Integer.MIN_VALUE;
-        int left = Math.max(0, helper(root.left));
-        int right = Math.max(0, helper(root.right));
+        int left = Math.max(0, traverse(root.left));
+        int right = Math.max(0, traverse(root.right));
         max = Math.max(max, root.val + left + right);
         return root.val + Math.max(left, right);
     }
