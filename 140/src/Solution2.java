7f25d5c62fed1c716f0ef97adf53028b67217d81
yunxiaozou <yunxiaozou@gmail.com>
2016-05-20 14:53:47 -0400

    342

---
+++ 140/src/Solution2.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by yunxiaozou on 5/20/16.
+ */
+public class Solution2 {
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        return DFS(s, wordDict, new HashMap<String, LinkedList<String>>());
+    }
+
+    // DFS function returns an array including all substrings derived from s.
+    List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>> map) {
+        if (map.containsKey(s))
+            return map.get(s);
+
+        LinkedList<String>res = new LinkedList<String>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+                for (String sub : sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+            }
+        }
+        map.put(s, res);
+        return res;
+    }
+}
