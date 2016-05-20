f9a47ca75db0661dd357b13911204f511c258f64
yunxiaozou <yunxiaozou@gmail.com>
2016-05-20 11:24:41 -0400

    342

---
+++ 136/src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+        for (int num: nums)
+            number = number ^ num;
+        return number;
+    }
+}
---
+++ 137/src/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Created by yunxiaozou on 5/20/16.
+ */
+public class Solution2 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            if(sum != 0) {
+                ans |= sum << i;
+            }
+        }
+        return ans;
+    }
+}
