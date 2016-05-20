8c6416741ebaf802c186f9d3bb9e83cb61f5877a
yunxiaozou <yunxiaozou@gmail.com>
2016-05-20 14:52:03 -0400

    342

--- 139/src/Solution02.java
+++ 139/src/Solution02.java
@@ -3,18 +3,16 @@ import java.util.Set;
 /**
  * Created by yunxiaozou on 5/20/16.
  */
-public class Solution2 {
+public class Solution02 {
     public boolean wordBreak(String s, Set<String> wordDict) {
-        // the maximum length;
         int maxWord = getMax(wordDict);
         int len = s.length();
-        boolean[] dp = new boolean[len + 1];
+        boolean[] dp = new boolean[len+1];
         dp[0] = true;
         for (int i = 1; i <= len; i++) {
-            int start = Math.max(1, i - maxWord);
+            int start = Math.max(1, i - maxWord+1);
             for (int j = start; j <= i; j++) {
-                // check cut [1...j-1] and [j,i] (1-based)
-                if (dp[j - 1] && wordDict.contains(s.substring(j - 1, i))) {
+                if (dp[j-1] && wordDict.contains(s.substring(j-1, i))) {
                     dp[i] = true;
                     break;
                 }
@@ -22,10 +20,9 @@ public class Solution2 {
         }
         return dp[len];
     }
-
     private int getMax(Set<String> wordDict) {
         int max = 0;
-        for (String str : wordDict) {
+        for (String str: wordDict) {
             max = Math.max(max, str.length());
         }
         return max;
