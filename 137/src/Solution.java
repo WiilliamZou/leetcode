2f744b84ada06db1fa2b610f380529d141eef033
yunxiaozou <yunxiaozou@gmail.com>
2016-05-20 11:45:57 -0400

    342

--- 137/src/Solution.java
+++ 137/src/Solution.java
@@ -1,5 +1,19 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        return 0;
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+
+            }
+            if (sum != 0) {
+                ans |=  1 << i;
+            }
+        }
+        return ans;
     }
 }
