e03a457556f59915932fe7ee6a1bbda6a07d0b2a
yunxiaozou <yunxiaozou@gmail.com>
2016-05-20 10:17:58 -0400

    342

--- 134/src/Solution.java
+++ 134/src/Solution.java
@@ -1,40 +1,22 @@
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int balance = 0;
-        int max = -1;
-        int local = 0;
-        int index = -1;
-        int localIndex = -1;
-        if (gas == null || cost == null || cost.length != gas.length) {
-            return -1;
-        }
         for (int i = 0; i < gas.length; i++) {
-            int gain = gas[i] - cost[i];
-            balance += gain;
-            if (gain >= 0) {
-                if (local == 0) {
-                    localIndex = i;
-                }
-                local += gain;
-                if (max < local){
-                    max = local;
-                    index = localIndex;
-                }
-            } else {
-                local = 0;
-            }
-        }
-        if (gas.length > 1) {
-            int gain = gas[0] - cost[0];
-            if (gain >= 0) {
-                local += gain;
-                if (max < local) {
-                    max = local;
-                    index = localIndex;
-                }
-            }
+            balance = balance + gas[i] - cost[i];
         }
         if (balance < 0) return -1;
-        else return index;
+
+        int accumulate = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int currGain = gas[i] - cost[i];
+            if (accumulate + currGain < 0) {
+                accumulate = 0;
+                start = i+1;
+            } else {
+                accumulate += currGain;
+            }
+        }
+        return start;
     }
 }
