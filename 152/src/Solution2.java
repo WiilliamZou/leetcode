c1d2451dab3af26fdcd7ac18a4a026b5a76ab44d
yunxiaozou <yunxiaozou@gmail.com>
2016-05-22 15:47:09 -0400

    342

---
+++ 152/src/Solution2.java
@@ -0,0 +1,22 @@
+/**
+ * Created by yunxiaozou on 5/22/16.
+ */
+public class Solution2 {
+    public int maxProduct(int[] A) {
+        int min;
+        int res = A[0], max = min = A[0]; // max, min means max and min product among the subarrays whose last element is A[i].
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > 0) {
+                max = Math.max(max * A[i], A[i]);
+                min = Math.min(min * A[i], A[i]);
+            }
+            else {
+                int lastMax = max;
+                max = Math.max(min * A[i], A[i]);
+                min = Math.min(lastMax * A[i], A[i]);
+            }
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+}
