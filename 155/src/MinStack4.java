28f73ed57ebf346410ce98fff6ebb5336e8e1683
yunxiaozou <yunxiaozou@gmail.com>
2016-05-22 20:46:05 -0400

    342

---
+++ 155/src/MinStack4.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+/**
+ * Created by yunxiaozou on 5/22/16.
+ */
+public class MinStack4 {
+    private Stack<Integer> st = new Stack<Integer>();
+    private Stack<Integer> min_st = new Stack<Integer>();
+
+    public void push(int x) {
+        if(min_st.isEmpty() || min_st.peek()>=x) min_st.push(x);
+        st.push(x);
+    }
+
+    public void pop() {
+        if((int)st.peek()==(int)min_st.peek()) min_st.pop();
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public int getMin() {
+        return min_st.peek();
+    }
+}
