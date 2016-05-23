d4a00fd303468e8835117c925d0065c34e2c73d5
yunxiaozou <yunxiaozou@gmail.com>
2016-05-23 11:11:57 -0400

    342

---
+++ 156/src/Solution3.java
@@ -0,0 +1,24 @@
+/**
+ * Created by yunxiaozou on 5/23/16.
+ */
+public class Solution3 {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode curr = root;
+        TreeNode next = null;
+        TreeNode temp = null;
+        TreeNode prev = null;
+
+        while(curr != null) {
+            next = curr.left;
+
+            // swapping nodes now, need temp to keep the previous right child
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
