4acead4d36ed5a95d00f42feebc952365c820dc8
yunxiaozou <yunxiaozou@gmail.com>
2016-05-23 18:36:14 -0400

    342

--- 159/src/Solution2.java
+++ 159/src/Solution2.java
@@ -3,23 +3,23 @@ import java.util.HashMap;
 public class Solution2 {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         if(s.length() < 1) return 0;
-        HashMap<Character,Integer> index = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
         int lo = 0;
         int hi = 0;
         int maxLength = 0;
         while(hi < s.length()) {
-            if(index.size() <= 2) {
+            if(map.size() <= 2) {
                 char c = s.charAt(hi);
-                index.put(c, hi);
+                map.put(c, hi);
                 hi++;
             }
-            if(index.size() > 2) {
+            if(map.size() > 2) {
                 int leftMost = s.length();
-                for(int i : index.values()) {
+                for(int i : map.values()) {
                     leftMost = Math.min(leftMost,i);
                 }
                 char c = s.charAt(leftMost);
-                index.remove(c);
+                map.remove(c);
                 lo = leftMost+1;
             }
             maxLength = Math.max(maxLength, hi-lo);
