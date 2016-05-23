886eb7c90c9dbd15a8462719f79eea0e04802ebd
yunxiaozou <yunxiaozou@gmail.com>
2016-05-23 16:34:30 -0400

    342

--- 156/src/Solution02.java
+++ 156/src/Solution02.java
@@ -2,4 +2,14 @@
  * Created by yunxiaozou on 5/23/16.
  */
 public class Solution02 {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = root.right = null;
+        return newRoot;
+    }
 }
---
+++ 157/src/Solution2.java
@@ -0,0 +1,28 @@
+/**
+ * Created by yunxiaozou on 5/23/16.
+ */
+public class Solution2 extends Reader4{
+    public int read(char[] buf, int n) {
+        boolean eof = false;      // end of file flag
+        int total = 0;            // total bytes have read
+        char[] tmp = new char[4]; // temp buffer
+
+        while (!eof && total < n) {
+            int count = read4(tmp);
+
+            // check if it's the end of the file
+            eof = count < 4;
+
+            // get the actual count
+            count = Math.min(count, n - total);
+
+            // copy from temp buffer to buf
+            for (int i = 0; i < count; i++)
+                buf[total++] = tmp[i];
+        }
+
+        return total;
+    }
+
+
+}
