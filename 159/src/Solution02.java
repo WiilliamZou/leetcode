b83167e349b4f45dd450b0f6fe2a097595b73e11
yunxiaozou <yunxiaozou@gmail.com>
2016-05-23 20:37:38 -0400

    342

--- 159/src/Solution02.java
+++ 159/src/Solution02.java
@@ -1,24 +1,25 @@
 import java.util.HashMap;
 
-public class Solution2 {
+/**
+ * Created by yunxiaozou on 5/23/16.
+ */
+public class Solution02 {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        // two pointers and map style.
-        if(s.length() < 1) return 0;
-        // the rightmost index of character
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        if (s.length() < 1) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
         int lo = 0;
         int hi = 0;
         int maxLength = 0;
-        while(hi < s.length()) {
-            if(map.size() <= 2) {
+        while (hi < s.length()) {
+            if (map.size() <= 2) {
                 char c = s.charAt(hi);
                 map.put(c, hi);
                 hi++;
             }
-            if(map.size() > 2) {
+            if (map.size() > 2){
                 int leftMost = s.length();
-                for(int i : map.values()) {
-                    leftMost = Math.min(leftMost,i);
+                for (int i : map.values()) {
+                    leftMost = Math.min(leftMost, i);
                 }
                 char c = s.charAt(leftMost);
                 map.remove(c);
