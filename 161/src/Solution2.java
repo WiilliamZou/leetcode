a30237bf34e9fe64f00755af41e02d3bec436012
yunxiaozou <yunxiaozou@gmail.com>
2016-05-23 20:56:03 -0400

    342

--- 161/src/Solution2.java
+++ 161/src/Solution2.java
@@ -18,11 +18,14 @@ public class Solution2 {
     public boolean isOneEditDistance(String s, String t) {
         for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
             if (s.charAt(i) != t.charAt(i)) {
-                if (s.length() == t.length()) // s has the same length as t, so the only possibility is replacing one char in s and t
+                if (s.length() == t.length())
+                    // s has the same length as t, so the only possibility is replacing one char in s and t
                     return s.substring(i + 1).equals(t.substring(i + 1));
-                else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
+                else if (s.length() < t.length())
+                    // t is longer than s, so the only possibility is deleting one char from t
                     return s.substring(i).equals(t.substring(i + 1));
-                else // s is longer than t, so the only possibility is deleting one char from s
+                else
+                    // s is longer than t, so the only possibility is deleting one char from s
                     return t.substring(i).equals(s.substring(i + 1));
             }
         }
