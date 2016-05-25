724d5696d6283ca79599614906402b3379a43573
yunxiaozou <yunxiaozou@gmail.com>
2016-05-25 13:16:57 -0400

    342

---
+++ 166/src/Solution2Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by yunxiaozou on 5/25/16.
+ */
+public class Solution2Test {
+    @org.junit.Test
+    public void fractionToDecimal() throws Exception {
+
+    }
+
+}
