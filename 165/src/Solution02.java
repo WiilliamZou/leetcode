b6fb5c3e93f789c233cf6df54c58990e6a02ac30
yunxiaozou <yunxiaozou@gmail.com>
2016-05-25 12:53:39 -0400

    342

---
+++ 165/src/Solution02.java
@@ -0,0 +1,16 @@
+/**
+ * Created by yunxiaozou on 5/25/16.
+ */
+public class Solution02 {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\."), v2 = version2.split("\\.");
+        int i;
+        for (i = 0; i < v1.length && i < v2.length; i++) {
+            if (Integer.parseInt(v1[i]) != Integer.parseInt(v2[i]))
+                return Integer.parseInt(v1[i]) > Integer.parseInt(v2[i]) ? 1: -1;
+        }
+        for (; i < v1.length; i++) if (Integer.parseInt(v1[i]) != 0) return 1;
+        for (; i < v2.length; i++) if (Integer.parseInt(v2[i]) != 0) return -1;
+        return 0;
+    }
+}
