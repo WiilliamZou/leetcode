aa0f2a269d8336ea040d7b0dca1b0ace0cd15b95
yunxiaozou <yunxiaozou@gmail.com>
2016-05-26 15:07:46 -0400

    342

---
+++ 189/src/Solution2.java
@@ -0,0 +1,19 @@
+/**
+ * Created by yunxiaozou on 5/26/16.
+ */
+public class Solution2 {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);  // reverse the whole array
+        reverse(nums, 0, k-1);  // reverse the first part
+        reverse(nums, k, nums.length-1);  // reverse the second part
+    }
+
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int tmp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = tmp;
+        }
+    }
+}
