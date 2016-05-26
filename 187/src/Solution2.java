f54a4453f77ac54acad17165c30f68fee96d778a
yunxiaozou <yunxiaozou@gmail.com>
2016-05-26 11:55:14 -0400

    342

---
+++ 187/src/Solution2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by yunxiaozou on 5/26/16.
+ */
+public class Solution2 {
+    // 理解 set 的 处理
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<String>();
+        Set<String> resset = new HashSet<String>();
+        if(s == null || s.length() <= 10){
+            return res;
+        }
+        Set<String> set = new HashSet<String>();
+        int len = s.length();
+        for(int i = 0; i <= len - 10; i++){
+            String sub = s.substring(i, i + 10);
+            if(!set.add(sub)){
+                resset.add(sub);
+            }
+        }
+        res.addAll(resset);
+        return res;
+    }
+}
