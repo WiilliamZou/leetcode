171ecb1b63d028177b7db548d30b725f7d92ff7e
yunxiaozou <yunxiaozou@gmail.com>
2016-05-27 14:40:15 -0400

    342

--- 209/src/Solution04.java
+++ 209/src/Solution04.java
@@ -1,24 +1,25 @@
-public class Solution4 {
+/**
+ * Created by yunxiaozou on 5/27/16.
+ */
+public class Solution04 {
     public int minSubArrayLen(int s, int[] nums) {
         int i = 1, j = nums.length, min = 0;
         while (i <= j) {
-            int mid = (i + j) / 2;
-            // binary-like search mid is the size.
-            // try different size.
-            // size range is [i.. j]. inclusive
+            int mid = (i+j)/2;
             if (windowExist(mid, nums, s)) {
-                j = mid - 1;
+                j = mid-1;
                 min = mid;
-            } else i = mid + 1;
+            } else {
+                i = mid + 1;
+            }
         }
         return min;
     }
 
-
     private boolean windowExist(int size, int[] nums, int s) {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i >= size) sum -= nums[i - size];
+            if (i >= size) sum -= nums[i-size];
             sum += nums[i];
             if (sum >= s) return true;
         }
