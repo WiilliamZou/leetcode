db831fedfb086062cf87029c0a3e0730e690f89b
yunxiaozou <yunxiaozou@gmail.com>
2016-05-27 19:22:04 -0400

    342

--- 215/src/Solution03.java
+++ 215/src/Solution03.java
@@ -1,34 +1,37 @@
+import java.util.PriorityQueue;
+
 /**
  * Created by yunxiaozou on 5/27/16.
  */
 public class Solution03 {
     public int findKthLargest(int[] nums, int k) {
         if (nums == null || nums.length == 0) return Integer.MAX_VALUE;
-        return findKthLargest(nums, 0, nums.length - 1, nums.length - k);
+        return findKthLargest(nums, 0, nums.length-1, nums.length-k);
     }
 
-    public int findKthLargest(int[] nums, int start, int end, int k) {// quick select: kth smallest
+    private int findKthLargest(int[] nums, int start, int end, int k) {
         if (start > end) return Integer.MAX_VALUE;
 
-        int pivot = nums[end];// Take A[end] as the pivot,
+        int pivot = nums[end];
         int left = start;
         for (int i = start; i < end; i++) {
-            if (nums[i] <= pivot) // Put numbers < pivot to pivot's left
-                swap(nums, left++, i);
+            if(nums[i] <= pivot) {
+                swap(nums, left++,i);
+            }
         }
-        swap(nums, left, end);// Finally, swap A[end] with A[left]
-
-        if (left == k)// Found kth smallest number
+        swap(nums,left,end);
+        if (left == k) {
             return nums[left];
-        else if (left < k)// Check right part
-            return findKthLargest(nums, left + 1, end, k);
-        else // Check left part
-            return findKthLargest(nums, start, left - 1, k);
+        } else if (left < k) {
+            return findKthLargest(nums, left+1, end, k);
+        } else {
+            return findKthLargest(nums, start, left-1, k);
+        }
     }
 
-    void swap(int[] A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
