1471c5c1d00799e5d3cc2981aaeab1af15c1a139
yunxiaozou <yunxiaozou@gmail.com>
2016-05-27 18:54:12 -0400

    342

--- 215/src/Solution2.java
+++ 215/src/Solution2.java
@@ -4,12 +4,12 @@ import java.util.PriorityQueue;
  * Created by yunxiaozou on 5/27/16.
  */
 public class Solution2 {
-    private int findKthLargest(int[] nums, int k) {
+    public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int num : nums) {
             pq.add(num);
-            if (pq.size() > k) pq.poll();
+            if (pq.size() > k) pq.remove();
         }
-        return pq.poll();
+        return pq.remove();
     }
 }
