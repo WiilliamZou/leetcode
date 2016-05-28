670ad295e06b5a5d22033f3aa8e1e8da34def5ff
yunxiaozou <yunxiaozou@gmail.com>
2016-05-28 10:50:53 -0400

    342

---
+++ 217/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i: nums) {
+            if (set.contains(i)) {
+                return true;
+            } else {
+                set.add(i);
+            }
+        }
+        return false;
+    }
+}
