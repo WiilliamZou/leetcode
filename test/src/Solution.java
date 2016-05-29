8d30838035c1f0afbeb40e3f3038bf542547e729
yunxiaozou <yunxiaozou@gmail.com>
2016-05-28 20:13:39 -0400

    342

---
+++ test/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+/**
+ * Created by yunxiaozou on 5/28/16.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String w = "java";
+        System.out.println(Arrays.toString(w.split("")));
+    }
+}
