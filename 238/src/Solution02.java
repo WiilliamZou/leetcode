d58daa8cf02c58943683dd26bebb8aa8de6bec62
yunxiaozou <yunxiaozou@gmail.com>
2016-05-29 18:43:38 -0400

    342

--- 238/src/Solution02.java
+++ 238/src/Solution02.java
@@ -1,13 +1,16 @@
-public class Solution2 {
+/**
+ * Created by yunxiaozou on 5/29/16.
+ */
+public class Solution02 {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         res[0] = 1;
         for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+            res[i] = res[i-1] * nums[i-1];
         }
         int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n -1; i >= 0; i--) {
             res[i] *= right;
             right *= nums[i];
         }
