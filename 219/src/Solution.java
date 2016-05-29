55d1ad6947b61b9de69e5ce1c9fe8897d8d05b24
yunxiaozou <yunxiaozou@gmail.com>
2016-05-28 20:19:40 -0400

    342

---
+++ 219/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        if (nums == null) return false;
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) return true;
+                else map.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
