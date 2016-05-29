be36cdb2dc9ecf6b42eb691c35b2792e20d881d4
yunxiaozou <yunxiaozou@gmail.com>
2016-05-29 10:24:48 -0400

    342

---
+++ 224/src/Solution02.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+import java.util.zip.CheckedInputStream;
+
+/**
+ * Created by yunxiaozou on 5/29/16.
+ */
+public class Solution02 {
+    public int calculate(String s) {
+        int len = s.length(), sign = -1, result = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i+1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
+    }
+}
