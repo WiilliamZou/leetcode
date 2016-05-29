f68328643c0ff4fc6927153fb68a027228e815e1
yunxiaozou <yunxiaozou@gmail.com>
2016-05-29 16:31:13 -0400

    342

---
+++ 235/src/Solution3.java
@@ -0,0 +1,12 @@
+public class Solution3 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val > q.val)
+                root = root.left;
+            else if (root.val < p.val && root.val < q.val)
+                root = root.right;
+            else
+                return root;
+        }
+    }
+}
