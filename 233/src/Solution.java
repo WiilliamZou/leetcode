568676eb738a29966d40f335e3664cad30bc2888
yunxiaozou <yunxiaozou@gmail.com>
2016-05-29 14:49:43 -0400

    342

---
+++ 233/src/Solution.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yunxiaozou on 5/29/16.
+ */
+public class Solution {
+}
---
+++ 233/src/Solution2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yunxiaozou on 5/29/16.
+ */
+public class Solution2 {
+    public int countDigitOne(int n) {
+        int ones = 0;
+        for (long m = 1; m <= n; m *= 10)
+            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
+        return ones;
+    }
+}
