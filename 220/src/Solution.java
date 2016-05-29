55120da999ea421e6a19137ff00890c5e9221665
yunxiaozou <yunxiaozou@gmail.com>
2016-05-28 20:29:36 -0400

    342

---
+++ 220/src/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null) return false;
+        return false;
+    }
+}
---
+++ 220/src/Solution02.java
@@ -0,0 +1,30 @@
+import java.util.TreeSet;
+
+/**
+ * Created by yunxiaozou on 5/28/16.
+ */
+public class Solution02 {
+    public boolean containsNearybyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+        TreeSet<Integer> values = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer floor = values.floor(nums[i]+t);
+            Integer ceil = values.ceiling(nums[i]-t);
+            if ((floor != null && floor >= nums[i]) ||
+                    (ceil != null && ceil <= nums[i])) {
+                return true;
+            }
+
+            values.add(nums[i]);
+            if (i >= k) {
+                values.remove(nums[i-k]);
+            }
+
+        }
+        return false;
+
+    }
+
+}
