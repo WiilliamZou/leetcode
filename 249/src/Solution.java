4bae269a6df52775f04f9bcca676c70e72ae35c6
yunxiaozou <yunxiaozou@gmail.com>
2016-05-30 15:08:00 -0400

    342

---
+++ 249/src/Solution.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    HashMap<String, List<String>> map = new HashMap<>();
+    public List<List<String>> groupStrings(String[] strings) {
+        return null;
+    }
+}
