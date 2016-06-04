9332215fc849b6e8017a9daa3f25c0df15af9c86
yunxiaozou <yunxiaozou@gmail.com>
2016-06-04 12:19:35 -0400

    342

--- 294/src/Solution2.java
+++ 294/src/Solution2.java
@@ -6,7 +6,7 @@ public class Solution2 {
         if (s == null || s.length() < 2) {
             return false;
         }
-
+        //like brute force.
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.startsWith("++", i)) {
                 String t = s.substring(0, i) + "--" + s.substring(i + 2);
