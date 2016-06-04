1140261e036b3b475e7ac5a2b7d3eb58ff24b8a4
yunxiaozou <yunxiaozou@gmail.com>
2016-06-04 11:30:58 -0400

    342

--- 294/src/Solution2.java
+++ 294/src/Solution2.java
@@ -2,4 +2,22 @@
  * Created by yunxiaozou on 6/4/16.
  */
 public class Solution2 {
+    public boolean canWin(String s) {
+        if (s == null || s.length() < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.startsWith("++", i)) {
+                String t = s.substring(0, i) + "--" + s.substring(i + 2);
+
+                if (!canWin(t)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
