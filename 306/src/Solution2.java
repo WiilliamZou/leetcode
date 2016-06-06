e413e63e21d831424761bac385525510eb49b8b2
yunxiaozou <yunxiaozou@gmail.com>
2016-06-05 20:33:14 -0400

    342

--- 306/src/Solution2.java
+++ 306/src/Solution2.java
@@ -3,11 +3,12 @@ public class Solution2 {
         if (num == null || num.length() < 3) return false;
         int n = num.length();
         for (int i = 1; i < n; i++) {
-            if (i > 1 && num.charAt(0) == '0') break;
+            if (i > 1 && num.charAt(0) == '0') break; // leading 0 is not allowed.
             for (int j = i+1; j < n; j++) {
                 // i, j means bound.
                 int first = 0, second = i, third = j;
                 if (num.charAt(second) == '0' && third > second+1) break;
+                // leading 0 is not allowd.
                 while (third < n) {
                     Long result = (Long.parseLong(num.substring(first, second)) +
                             Long.parseLong(num.substring(second, third)) );
