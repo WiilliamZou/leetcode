f68a33b9f3b3805158118be14811074f70303e31
yunxiaozou <yunxiaozou@gmail.com>
2016-06-06 17:21:32 -0400

    342

--- 314/src/Solution.java
+++ 314/src/Solution.java
@@ -1,32 +1,27 @@
 import java.util.*;
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
         Map<Integer, List<Integer>> map = new TreeMap<>();
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(root, 0));
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> col = new LinkedList<>();
+        queue.add(root);
+        col.add(0);
         while(!queue.isEmpty()) {
-            Node node = queue.remove();
-            if (!map.containsKey(node.i)) {
-                map.put(node.i, new ArrayList<>());
+            TreeNode node = queue.remove();
+            int i = col.remove();
+            if (!map.containsKey(i)) {
+                map.put(i, new ArrayList<>());
             }
-            map.get(node.i).add(node.treeNode.val);
-            if (node.treeNode.left != null) {
-                queue.add(new Node(node.treeNode.left, node.i-1));
+            map.get(i).add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+                col.add(i-1);
             }
-            if (node.treeNode.right != null) {
-                queue.add(new Node(node.treeNode.right, node.i+1));
+            if (node.right != null) {
+                queue.add(node.right);
+                col.add(i+1);
             }
         }
         for (int key: map.keySet()) {
@@ -34,14 +29,4 @@ public class Solution {
         }
         return result;
     }
-    private class Node {
-        TreeNode treeNode;
-        int i;
-        public Node(TreeNode treeNode, int i) {
-            this.treeNode = treeNode;
-            this.i = i;
-        }
-    }
-
-
 }
