c337a8e3aa8ebd7c99087f36f068e59bc5a2791b
yunxiaozou <yunxiaozou@gmail.com>
2016-06-06 20:58:05 -0400

    dfda

--- 317/src/Solution02.java
+++ 317/src/Solution02.java
@@ -1,47 +1,38 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Solution2 {
+/**
+ * Created by yunxiaozou on 6/6/16.
+ */
+public class Solution02 {
     public int shortestDistance(int[][] grid) {
-        if (grid == null || grid[0].length == 0) return 0;
+        if (grid == null ||grid.length == 0 || grid[0].length == 0) return 0;
         final int[] shift = new int[] {0, 1, 0, -1, 0};
-
-        int row  = grid.length, col = grid[0].length;
+        int row = grid.length, col = grid[0].length;
         int[][] distance = new int[row][col];
         int[][] reach = new int[row][col];
         int buildingNum = 0;
-
         for (int i = 0; i < row; i++) {
-            for (int j =0; j < col; j++) {
+            for (int j = 0; j < col; j++) {
                 if (grid[i][j] == 1) {
-                    // start from a building.
                     buildingNum++;
-                    // int[] 中应该是相应的坐标。
-                    Queue<int[]> myQueue = new LinkedList<int[]>();
-                    //start from the coordianate of building.
-                    myQueue.offer(new int[] {i,j});
-
-                    boolean[][] isVisited = new boolean[row][col];
+                    Queue<int[]> myQueue = new LinkedList<>();
+                    myQueue.add(new int[] {i,j});
+                    boolean[][] isVisisted = new boolean[row][col];
                     int level = 1;
-
                     while (!myQueue.isEmpty()) {
                         int qSize = myQueue.size();
                         for (int q = 0; q < qSize; q++) {
-                            int[] curr = myQueue.poll();
-
+                            int[] curr = myQueue.remove();
                             for (int k = 0; k < 4; k++) {
                                 int nextRow = curr[0] + shift[k];
-                                int nextCol = curr[1] + shift[k + 1];
-
+                                int nextCol = curr[1] + shift[k+1];
                                 if (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col
-                                        && grid[nextRow][nextCol] == 0 && !isVisited[nextRow][nextCol]) {
-                                    //The shortest distance from [nextRow][nextCol] to thic building
-                                    // is 'level'.
+                                        && grid[nextRow][nextCol] == 0 && !isVisisted[nextRow][nextCol]) {
                                     distance[nextRow][nextCol] += level;
                                     reach[nextRow][nextCol]++;
-
-                                    isVisited[nextRow][nextCol] = true;
-                                    myQueue.offer(new int[] {nextRow, nextCol});
+                                    isVisisted[nextRow][nextCol] = true;
+                                    myQueue.add(new int[]{nextRow, nextCol});
                                 }
                             }
                         }
@@ -50,7 +41,6 @@ public class Solution2 {
                 }
             }
         }
-
         int shortest = Integer.MAX_VALUE;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -59,9 +49,6 @@ public class Solution2 {
                 }
             }
         }
-
-        return shortest == Integer.MAX_VALUE ? -1 : shortest;
-
-
+        return shortest == Integer.MAX_VALUE ? -1: shortest;
     }
 }
