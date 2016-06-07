8cd48ad9f7aae5f2985617b00d4fcedcd77ced28
yunxiaozou <yunxiaozou@gmail.com>
2016-06-06 21:16:28 -0400

    dfda

---
+++ 318/src/Solution2.java
@@ -0,0 +1,19 @@
+public class Solution2 {
+    public int maxProduct(String[] words) {
+        int max = 0;
+        int[] bytes = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            int val = 0;
+            for (int j = 0; j < words[i].length(); j++) {
+                val |= 1 << (words[i].charAt(j) - 'a');
+            }
+            bytes[i] = val;
+        }
+        for (int i = 0; i < bytes.length; i++) {
+            for (int j = i + 1; j < bytes.length; j++) {
+                if ((bytes[i] & bytes[j]) == 0) max = Math.max(max, words[i].length() * words[j].length());
+            }
+        }
+        return max;
+    }
+}
