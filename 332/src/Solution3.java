c633e7278e5d629155d549e27d02430efd41befd
yunxiaozou <yunxiaozou@gmail.com>
2016-06-08 19:59:53 -0400

    dfda

---
+++ 332/src/Solution3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * Created by yunxiaozou on 6/8/16.
+ */
+public class Solution3 {
+    public List<String> findItinerary(String[][] tickets) {
+        Map<String, PriorityQueue<String>> targets = new HashMap<>();
+        for (String[] ticket : tickets)
+            targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+        List<String> route = new LinkedList();
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+        while (!stack.empty()) {
+            while (targets.containsKey(stack.peek()) && !targets.get(stack.peek()).isEmpty())
+                stack.push(targets.get(stack.peek()).poll());
+            route.add(0, stack.pop());
+        }
+        return route;
+    }
+
+}
