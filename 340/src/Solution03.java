e77f52d46af1dd1ee57b4c601b271a6c9e05624b
yunxiaozou <yunxiaozou@gmail.com>
2016-06-09 19:38:41 -0400

    dfad

---
+++ 340/src/Solution03.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yunxiaozou on 6/9/16.
+ */
+public class Solution03 {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] count = new int[256];
+        int num = 0, i = 0, res = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (count[s.charAt(j)]++ == 0) num++;
+            if (num > k) {
+                while (--count[s.charAt(i++)] > 0);
+                num--;
+            }
+            res = Math.max(res, j-i+1);
+        }
+        return res;
+    }
+}
