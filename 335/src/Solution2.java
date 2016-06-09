ababe3ef680f8c690a2812ab26e62ac74c91a397
yunxiaozou <yunxiaozou@gmail.com>
2016-06-09 14:05:06 -0400

    dfad

--- 335/src/Solution2.java
+++ 335/src/Solution2.java
@@ -7,7 +7,7 @@ public class Solution2 {
         int l = x.length;
         if(l <= 3) return false;
 
-        for(int i = 3; i < l; i++){
+        for(int i = 3; i < l; i++){ //从第四条线算起`
             if(x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;  //Fourth line crosses first line and onward
             if(i >=4)
             {
@@ -15,7 +15,9 @@ public class Solution2 {
             }
             if(i >=5)
             {
-                if(x[i-2] - x[i-4] >= 0 && x[i] >= x[i-2] - x[i-4] && x[i-1] >= x[i-3] - x[i-5] && x[i-1] <= x[i-3]) return true;  // Sixth line crosses first line and onward
+                if(x[i-2] - x[i-4] >= 0 && x[i] >= x[i-2] - x[i-4]
+                        && x[i-1] >= x[i-3] - x[i-5]
+                        && x[i-1] <= x[i-3]) return true;  // Sixth line crosses first line and onward
             }
         }
         return false;
