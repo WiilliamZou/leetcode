c006828cfe3ff3b16058c0ab7138e859376f30d1
yunxiaozou <yunxiaozou@gmail.com>
2016-07-01 19:07:25 -0400

    dfad

--- 029/src/Solution2.java
+++ 029/src/Solution2.java
@@ -3,26 +3,27 @@
  */
 public class Solution2 {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1)
-            return Integer.MAX_VALUE;
-
         long divd = Math.abs((long)dividend);
         long divs = Math.abs((long)divisor);
 
-        int ret = 0;
+        long ret = 0;
         while (divd >= divs) {
             int counter = 0;
             while (divd >= (divs << counter)) { // keep multiply by 2 until divs > divd
                 counter++;
             }
             counter--;  // rollback counter so that (divs<<counter) <= divd
-            ret += 1 << counter;  // quotient
+            ret += 1L << counter;  // quotient
             divd -= divs << counter;
         }
 
-        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
-            return ret;
-        else
-            return -ret;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            ret = -ret;
+        System.out.print(ret);
+        if (ret > Integer.MAX_VALUE || ret < Integer.MIN_VALUE) {
+            return Integer.MAX_VALUE;
+        } else  {
+            return (int) ret;
+        }
     }
 }
---
+++ 029/src/Solution2Test.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yunxiaozou on 7/1/16.
+ */
+public class Solution2Test {
+    @org.junit.Test
+    public void divide() throws Exception {
+        Solution2 sol = new Solution2();
+        sol.divide(-2147483648,-1);
+    }
+
+}
